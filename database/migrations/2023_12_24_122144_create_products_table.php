a90861f8e4386059ef6a3fbc6b8af3cf12a3a488
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2023-12-24 19:00:50 +0600

    Add migration to create products table

---
+++ database/migrations/2023_12_24_122144_create_products_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title')->index();
+            $table->text('short_description')->nullable();
+            $table->text('description')->nullable();
+            $table->double('price', 8, 2)->default(0);
+            $table->double('sale_price', 8, 2)->nullable();
+            $table->string('sku', 255)->nullable()->unique();
+            $table->string('photo')->nullable();
+
+            $table->boolean('onsale')->default(false);
+            
+            $table->foreignId('user_id')->constrained();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+        });
+
+        Schema::dropIfExists('products');
+    }
+};
