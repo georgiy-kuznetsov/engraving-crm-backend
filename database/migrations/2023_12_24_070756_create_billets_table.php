1496fd9bcc8d6c022cf865df98ff29b161deb0bd
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2023-12-24 15:34:06 +0600

    Add migration to create billets table

---
+++ database/migrations/2023_12_24_070756_create_billets_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('billets', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name', 255);
+            $table->double('price', 8, 2)->default(0);
+            $table->text('description')->nullable();
+            $table->string('sku', 255)->nullable()->unique();
+            $table->string('photo')->nullable();
+            $table->integer('stock_quantity')->default(0);
+
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('provider_id')->constrained();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('billets', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropForeign(['provider_id']);
+        });
+
+        Schema::dropIfExists('billets');
+    }
+};
