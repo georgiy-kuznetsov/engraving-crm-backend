7d2e0ef306481b172923566cd0fed695fa11a4dd
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2023-12-25 20:24:57 +0600

    Add migration to create orders table
    Migration adds the orders table to the database, which stores information about orders.
    The table has the foreign keys customer_id of the customers table and user_id of the users table.

---
+++ database/migrations/2023_12_25_140228_create_orders_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('number')->unique();
+            $table->double('price_amount', 8, 2)->default(0);
+            $table->double('discount_amount', 8, 2)->default(0);
+            $table->double('shipping_amount', 8, 2)->default(0);
+            $table->double('gratuity_amount', 8, 2)->default(0);
+            $table->double('total_amount', 8, 2)->default(0);
+
+            $table->foreignId('customer_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
