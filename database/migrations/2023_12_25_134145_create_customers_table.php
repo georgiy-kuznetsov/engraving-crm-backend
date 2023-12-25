15724b703db9b77f88311299cca8b266af135da9
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2023-12-25 19:57:40 +0600

    Add migration to create customers table
    Migration creates a customers table with the user_id foreign key of the users table

---
+++ database/migrations/2023_12_25_134145_create_customers_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name')->unique();
+            $table->string('surname')->nullable()->unique();
+            $table->string('phone')->nullable()->unique();
+            $table->string('email', 100)->nullable()->unique();
+
+            $table->string('country')->nullable();
+            $table->string('region')->nullable();
+            $table->string('city')->nullable();
+            $table->string('adress')->nullable();
+            $table->string('postcode', 20)->nullable();
+
+            $table->string('website')->nullable()->unique();
+            $table->string('telegram')->nullable();
+            $table->string('vkontakte')->nullable()->unique();
+            $table->string('instagram')->nullable()->unique();
+            $table->string('whatsapp')->nullable();
+
+            $table->boolean('is_banned')->default(false);
+            $table->boolean('is_regular')->default(false);
+
+            $table->foreignId('user_id')->constrained();
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
