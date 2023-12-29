fdb753b31ce9939bb44120fa091504a05bd9d34e
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2023-12-29 21:56:27 +0600

    Change migration to users table
    The following changes have been made to the migration:
    - column "name" changed to "first_name"
    - column "surname" changed to "last_name"
    - column "active" added - flag indicating that the user is active

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,30 +6,29 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->id()->from(3001);
+
             $table->string('login')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('name')->nullable();
-            $table->string('surname')->nullable();
+
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
             $table->string('avatar_large')->nullable();
             $table->string('avatar_small')->nullable();
+
+            $table->boolean('active')->default(true);
+
             $table->rememberToken();
             $table->timestamps();
         });
 
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('users');
