efb68a99b24e2028305eb300027314931cd26005
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2023-12-30 19:36:00 +0600

    The 'solors' table has been renamed to 'colors'

--- database/migrations/2023_12_26_144549_create_colors_table.php
+++ database/migrations/2023_12_26_144549_create_colors_table.php
@@ -8,7 +8,7 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('solors', function (Blueprint $table) {
+        Schema::create('colors', function (Blueprint $table) {
             $table->id();
             $table->string('rgb');
             $table->timestamps();
@@ -17,6 +17,6 @@ return new class extends Migration
 
     public function down(): void
     {
-        Schema::dropIfExists('solors');
+        Schema::dropIfExists('colors');
     }
 };
