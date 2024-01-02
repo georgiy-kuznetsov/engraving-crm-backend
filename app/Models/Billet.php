ad754037f5ed2e8b5dee120205d801f0ae730bf3
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2024-01-02 20:09:50 +0600

    Add model for the 'billets' table

---
+++ app/Models/Billet.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Billet extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'price',
+        'description',
+        'sku',
+        'quantity_stock',
+    ];
+
+    protected $hidden = [
+        'user_id',
+        'provider_id',
+    ];
+
+    protected $guarded = [
+        'photo_link',
+    ];
+}
