c0fc4b816614204fa72f3ee34bf2a1a7df973f39
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2024-01-03 10:15:11 +0600

    Fixed relationship for the 'User' and 'Provider'
    The relationships for 'User' and 'Provider' were defined
    in the 'User' and 'Billet' models. Fixed it.

--- app/Models/Billet.php
+++ app/Models/Billet.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Billet extends Model
 {
@@ -28,8 +27,5 @@ class Billet extends Model
     ];
 
 
-    public function user(): BelongsTo
-    {
-        return $this->belongsTo(User::class);
-    }
+    
 }
--- app/Models/Provider.php
+++ app/Models/Provider.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Provider extends Model
 {
@@ -30,4 +31,9 @@ class Provider extends Model
     ];
 
     protected $guarded = [];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
