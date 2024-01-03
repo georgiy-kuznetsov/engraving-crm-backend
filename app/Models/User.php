a3ad0cb59eec5615de12b78d75a916bbfd5b3ccf
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2024-01-03 16:51:02 +0600

    Add User Registration
    The UserController controller has been added, which includes
    the user registration method.
    An api route for user registration has also been added.

--- app/Models/User.php
+++ app/Models/User.php
@@ -25,6 +25,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'is_owner',
+        'active'
     ];
 
     protected $garded = [
