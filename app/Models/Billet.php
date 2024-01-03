daadc699d8fe434fbd9155377a293d891123f175
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2024-01-03 10:08:32 +0600

    Define external links for the 'User', 'Probider'
    For the 'User' and 'Probider' models, external links are defined
    as one to many.

--- app/Models/Billet.php
+++ app/Models/Billet.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Billet extends Model
 {
@@ -25,4 +26,10 @@ class Billet extends Model
     protected $guarded = [
         'photo_link',
     ];
+
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
