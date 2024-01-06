59d2bac5896c3b02b5ad80fe276e1e47fdf64a1d
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2024-01-06 18:30:30 +0600

    Replacing the registration() method
    The registration() method in the AuthController controller has been
    replaced by the store method in the UserController controller.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,7 +19,31 @@ class UserController extends Controller
 
     public function store(Request $request)
     {
-        //
+        $validatedData = $request->validate([
+            'login' => ['required', 'unique:users', 'min:7', 'max:50'],
+            'email' => ['required', 'string', 'email', 'unique:users'],
+            'password' => ['required', 'string', 'confirmed', 'min:6', 'max:50'],
+
+            'first_name' => ['nullable', 'string', 'max:100'],
+            'last_name' => ['nullable', 'string', 'max:100'],
+        ]);
+
+        $user = User::create([
+            'login' => $validatedData['login'],
+            'email' => $validatedData['email'],
+            'password' => bcrypt( $validatedData['password'] ),
+
+            'first_name' => $validatedData['first_name'],
+            'last_name' => $validatedData['last_name'],
+
+            'avatar_large' => null,
+            'avatar_small' => null,
+
+            'is_owner' => false,
+            'active' => false,
+        ]);
+
+        return response()->json($user);
     }
 
     public function show(User $user)
