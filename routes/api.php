1fa6f6002f6265d3a88939dccfe8b7d4693379bc
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2024-01-08 15:50:51 +0600

    Refactoring the router api

--- routes/api.php
+++ routes/api.php
@@ -4,21 +4,17 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::middleware('api')
+    ->group( function () {
+        Route::post('/registration', [AuthController::class, 'registration'])
+            ->name('registration');
 
-Route::post('/registration', [AuthController::class, 'registration'])->name('registration');
+        Route::post('/login', [AuthController::class, 'login'])
+            ->name('login');
 
-Route::post('/login', [AuthController::class, 'login'])->name('login');
+        Route::post('/logout', [AuthController::class, 'logout'])
+            ->name('logout')->middleware(['auth:sanctum']);
+    });
 
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware(['auth:sanctum']);
+Route::apiResource('/users', UserController::class)->middleware(['api', 'auth:sanctum']);
 
-Route::apiResource('/users', UserController::class)->middleware(['auth:sanctum']);
