789f7ac98e5e32f3a2fb3f86f7a0d461e372df4a
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2024-01-09 19:58:47 +0600

    Add manyToMany relationship for Product and Billet

--- app/Models/Billet.php
+++ app/Models/Billet.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Billet extends Model
 {
@@ -32,4 +33,8 @@ class Billet extends Model
     {
         return $this->belongsTo(User::class);
     }
-}
+
+    public function products(): BelongsToMany
+    {
+        return $this->belongsToMany(Product::class)->withPivot('quantity');
+    }}
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Product extends Model
 {
@@ -32,5 +33,10 @@ class Product extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function billets(): BelongsToMany
+    {
+        return $this->belongsToMany(Billet::class)->withPivot('quantity');
     }
 }
