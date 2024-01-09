fce6756fbcc6bc2376eaf2b24de601283de077bd
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2024-01-09 20:24:17 +0600

    Add controllers and routers for the Product model
    Added:
    - ProductController for implementing the CRUD Product model;
    - ProductBilletController for viewing the list, adding,
    deleting blanks of goods;
    - the corresponding routes.

---
+++ app/Http/Controllers/Product/ProductBilletController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Product;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProductBilletController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+    
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,12 @@
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegistrationController;
+use App\Http\Controllers\Product\ProductBilletController;
+use App\Http\Controllers\Product\ProductController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('api')
-    ->group( function () {
+Route::middleware('api')->group( function () {
         Route::post('/registration', [RegistrationController::class, 'registration'])
             ->name('registration');
 
@@ -16,7 +17,17 @@ Route::middleware('api')
 
         Route::post('/logout', [LogoutController::class, 'logout'])
             ->name('logout')->middleware(['auth:sanctum']);
-    });
+});
+
 
 Route::apiResource('/users', UserController::class)->middleware(['api', 'auth:sanctum']);
+
+
+Route::prefix('product')->group( function () {
+    Route::apiResource('/products', ProductController::class);
+
+    Route::get('/{id}/billet', [ProductBilletController::class, 'index']);
+    Route::post('/{id}/billet/{billet_id}', [ProductBilletController::class, 'store']);
+    Route::delete('/{id}/billet/{billet_id}', [ProductBilletController::class, 'destroy']);
+})->middleware(['api', 'auth:sanctum']);
 
