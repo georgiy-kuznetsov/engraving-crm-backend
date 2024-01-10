065fdfed6fcccebc0a509aa58adf7c6cd9527d0d
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2024-01-10 20:46:06 +0600

    Add store() method for BilletController

--- app/Http/Controllers/Billet/BilletController.php
+++ app/Http/Controllers/Billet/BilletController.php
@@ -29,7 +29,31 @@ class BilletController extends Controller
 
     public function store(Request $request)
     {
-        //
+        $validatedData = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'price' => ['required', 'decimal:2', 'max:99999999.99'],
+            'description' => ['nullable', 'string'],
+            'sku' => ['nullable', 'string', 'max:255'],
+            'stock_quantity' => ['required', 'integer', 'min:0'],
+            'provider_id' => ['nullable', 'integer', 'exists:providers,id'],
+        ]);
+
+        $billet = Billet::create([
+            'name' => $validatedData['name'],
+            'price' => $validatedData['price'],
+            'description' => $validatedData['description'],
+            'sku' => $validatedData['sku'],
+            'stock_quantity' => $validatedData['stock_quantity'],
+            'provider_id' => $validatedData['provider_id'],
+            'user_id' => $request->user()->id,
+            'photo' => null,
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'statusCode' => 201,
+            'item' => $billet,
+        ], 201);
     }
 
     public function show(string $id)
