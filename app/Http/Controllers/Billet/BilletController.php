836a36a95c4cf404f32d6e2d5cb0cc7aef2dc99b
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2024-01-10 20:39:24 +0600

    Add the index() method for BilletController

--- app/Http/Controllers/Billet/BilletController.php
+++ app/Http/Controllers/Billet/BilletController.php
@@ -3,13 +3,28 @@
 namespace App\Http\Controllers\Billet;
 
 use App\Http\Controllers\Controller;
+use App\Models\Billet;
 use Illuminate\Http\Request;
 
 class BilletController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $page = (int) $request->input('page') ?? 1;
+        $pageSize = (int) $request->input('pageSize') ?? env('API_ITEMS_PER_PAGE');
+
+        $billetItems = Billet::paginate($pageSize, ['*'], 'page', $page);
+
+        $data = [
+            'page' => $billetItems->currentPage(),
+            'pageSize' => $billetItems->perPage(),
+            'total' => $billetItems->total(),
+            'items' => $billetItems->items(),
+            'success' => true,
+            'statusCode' => 200,
+        ];
+
+        return response()->json($data, 200);
     }
 
     public function store(Request $request)
