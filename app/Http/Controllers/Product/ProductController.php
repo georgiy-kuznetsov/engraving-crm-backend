ef2a4a017abef5bdba6eb5a9123ddcccf839185c
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2024-01-13 12:04:30 +0600

    Add the index() method for ProductController

--- app/Http/Controllers/Product/ProductController.php
+++ app/Http/Controllers/Product/ProductController.php
@@ -2,14 +2,28 @@
 
 namespace App\Http\Controllers\Product;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\BaseController;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
-class ProductController extends Controller
+class ProductController extends BaseController
 {
-    public function index()
+    public function index(Request $request)
     {
-        //
+        
+        $page = (int) $request->input('page') ?? 1;
+        $pageSize = (int) $request->input('pageSize') ?? env('API_ITEMS_PER_PAGE');
+
+        $productItems = Product::paginate($pageSize, ['*'], 'page', $page);
+
+        $data = [
+            'page' => $productItems->currentPage(),
+            'pageSize' => $productItems->perPage(),
+            'total' => $productItems->total(),
+            'items' => $productItems->items(),
+        ];
+
+        return $this->sendSuccessResponse($data, 200);
     }
 
     public function store(Request $request)
