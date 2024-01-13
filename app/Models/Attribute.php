06b362fea868de42deb448c2d22fd1af5b84d806
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2024-01-13 21:14:57 +0600

    Add the Attribute model

---
+++ app/Models/Attribute.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Attribute extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name', 
+        'unit',
+    ];
+
+    // public function product() {
+    //     return $this->belongsToMany(Product::class)
+    //                 ->withPivot('value')
+    //                 ->withTimestamps();
+    // }
+}
