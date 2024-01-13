d91f8e37da42646b81ad9ae3264ecb3d9242991f
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2024-01-13 14:32:38 +0600

    Add index() method for ProductBilletController

--- app/Http/Controllers/Product/ProductBilletController.php
+++ app/Http/Controllers/Product/ProductBilletController.php
@@ -2,14 +2,23 @@
 
 namespace App\Http\Controllers\Product;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\BaseController;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
-class ProductBilletController extends Controller
+class ProductBilletController extends BaseController
 {
-    public function index()
+    public function index(Request $request, $productId)
     {
-        //
+        $product = Product::find($productId);
+
+        if (! $product) {
+            return $this->sendErrorResponse(['Product not found'], 404);
+        };
+
+        $billets = $product->billets;
+
+        return $this->sendSuccessResponse($billets, 200);
     }
     
     public function store(Request $request)
