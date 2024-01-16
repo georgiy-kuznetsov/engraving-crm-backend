8a3c75f6e15ac6c9f5117ff39af36fdb86cad0ee
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2024-01-16 21:46:55 +0600

    Add index() method for CustomerController

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -9,7 +9,17 @@ class CustomerController extends BaseController
 {
     public function index(Request $request)
     {
-        //
+        $page = (int) $request->input('page') ?? 1;
+        $pageSize = (int) $request->input('pageSize') ?? env('API_ITEMS_PER_PAGE');
+
+        $customerItems = Customer::paginate($pageSize, ['*'], 'page', $page);
+
+        return $this->sendSuccessResponse([
+            'page' => $customerItems->currentPage(),
+            'pageSize' => $customerItems->perPage(),
+            'total' => $customerItems->total(),
+            'items' => $customerItems->items(),
+        ], 200);
     }
 
     public function store(Request $request)
