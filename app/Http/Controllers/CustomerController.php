0d1971fb8c4ec71fcb1c378992d12e38feed1ab3
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2024-01-16 21:57:47 +0600

    Add store() method for CustomerController

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -24,7 +24,33 @@ class CustomerController extends BaseController
 
     public function store(Request $request)
     {
-        //
+        $validatedData = $request->validate([
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name' => ['nullable', 'string', 'max:255'],
+            'email' => ['nullable', 'string', 'email', 'unique:customers', 'max:100'],
+            'phone' => ['nullable', 'string', 'max:255'],
+            
+            'country' => ['nullable', 'string', 'max:255'],
+            'region' => ['nullable', 'string', 'max:255'],
+            'city' => ['nullable', 'string', 'max:255'],
+            'adress' => ['nullable', 'string', 'max:255'],
+            'postcode' => ['nullable', 'string', 'max:20'],
+
+            'website' => ['nullable', 'string', 'unique:providers', 'max:256'],
+            'telegram' => ['nullable', 'string', 'unique:providers', 'max:256'],
+            'vkontakte' => ['nullable', 'string', 'unique:providers', 'max:256'],
+            'instagram' => ['nullable', 'string', 'unique:providers', 'max:256'],
+            'whatsapp' => ['nullable', 'string', 'unique:providers', 'max:256'],
+        ]);
+
+        $customer = Customer::create([
+            ...$validatedData,
+            'user_id' => $request->user()->id,
+            'is_banned' => false,
+            'is_regular' => false,
+        ]);
+
+        return $this->sendSuccessResponse($customer, 201);
     }
 
     public function show(string $id)
