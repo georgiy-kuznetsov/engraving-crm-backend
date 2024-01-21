c9be636a0da1ecee6350a71b516d7002d67fe665
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2024-01-21 19:14:32 +0600

    Add store() method for Order\StatusController

--- app/Http/Controllers/Order/StatusController.php
+++ app/Http/Controllers/Order/StatusController.php
@@ -16,7 +16,15 @@ class StatusController extends BaseController
 
     public function store(Request $request)
     {
-        //
+        $validatedData = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'description' => ['nullable', 'string'],
+            'index' => ['nullable', 'integer', 'min:0'],
+        ]);
+
+        $status = Status::create($validatedData);
+
+        return $this->sendSuccessResponse($status, 200);
     }
 
     public function show(string $id)
