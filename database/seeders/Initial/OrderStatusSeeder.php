df43de773200058e9ac72d93b8e23df24ce3ff2f
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2024-02-11 23:05:19 +0600

    Delete the trashed() modificator in the OrderStatusSeeder

--- database/seeders/Initial/OrderStatusSeeder.php
+++ database/seeders/Initial/OrderStatusSeeder.php
@@ -14,66 +14,66 @@ class OrderStatusSeeder extends Seeder
             'name' => 'Создан',
             'description' => null,
             'index' => 1,
-        ])->trashed()->create();
+        ])->create();
         
         Status::factory()->state([
             'name' => 'В обработке',
             'description' => null,
             'index' => 2,
-        ])->trashed()->create();
+        ])->create();
 
         Status::factory()->state([
             'name' => 'Эскиз согласован',
             'description' => null,
             'index' => 3,
-        ])->trashed()->create();
+        ])->create();
 
         Status::factory()->state([
             'name' => 'Изготовлен',
             'description' => null,
             'index' => 4,
-        ])->trashed()->create();
+        ])->create();
 
         Status::factory()->state([
             'name' => 'Собран',
             'description' => null,
             'index' => 5,
-        ])->trashed()->create();
+        ])->create();
 
         Status::factory()->state([
             'name' => 'Передан в доставку',
             'description' => null,
             'index' => 6,
-        ])->trashed()->create();
+        ])->create();
 
         Status::factory()->state([
             'name' => 'Отправлен',
             'description' => null,
             'index' => 7,
-        ])->trashed()->create();
+        ])->create();
 
         Status::factory()->state([
             'name' => 'Доставлен',
             'description' => null,
             'index' => 8,
-        ])->trashed()->create();
+        ])->create();
 
         Status::factory()->state([
             'name' => 'Претензия',
             'description' => null,
             'index' => 9,
-        ])->trashed()->create();
+        ])->create();
 
         Status::factory()->state([
             'name' => 'Отменен',
             'description' => null,
             'index' => 10,
-        ])->trashed()->create();
+        ])->create();
 
         Status::factory()->state([
             'name' => 'Возвращен',
             'description' => null,
             'index' => 11,
-        ])->trashed()->create();
+        ])->create();
     }
 }
