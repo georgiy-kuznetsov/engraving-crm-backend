9879bc711b8503b39cf769785d470a4f728def8c
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2024-02-11 16:05:39 +0600

    Add the InitialDatabaseSeeder for start to seed initial data to database

---
+++ database/seeders/Initial/InitialDatabaseSeeder.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Database\Seeders\Initial;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class InitialDatabaseSeeder extends Seeder
+{
+    public function run(): void
+    {
+        //
+    }
+}
