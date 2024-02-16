4752bcb92c0337c02fb03f613eb2d433b7039282
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2024-02-16 18:17:39 +0600

    Create the service for the LoginController

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,27 +4,22 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
-use App\Models\User;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\ValidationException;
+use App\Service\Auth\LoginService;
 
 class LoginController extends Controller
 {
+    protected $service;
+    
+    public function __construct(LoginService $service)
+    {
+        $this->service = $service;
+    }
+    
     public function login(LoginRequest $request) {
         $validatedData = $request->validated();
 
-        $user = User::where('login', $validatedData['login'])->first();
-
-        if (!$user) {
-            $user = User::where('email', $validatedData['login'])->first();
-        }
-        
-        if ( !$user || !Hash::check($validatedData['password'], $user->password) ) {
-            throw ValidationException::withMessages([
-                'login' => 'Invalid username or password.',
-            ]);
-        };
-
-        return [ 'token' => $user->createToken('api-token')->plainTextToken ];
+        return [
+            'token' => $this->service->login($validatedData),
+        ];
     }
 }
---
+++ app/Service/Auth/LoginService.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Service\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class LoginService extends Controller {
+    public function login($data) {
+        if ( ! $user = User::where('login', $data['login'])->first() ) {
+            $user = User::where('email', $data['login'])->first();
+        }
+        
+        if ( !$user || !Hash::check($data['password'], $user->password) ) {
+            throw ValidationException::withMessages([
+                'login' => 'Invalid username or password.',
+            ]);
+        };
+
+        return $user->createToken('api-token')->plainTextToken;
+    }
+}
