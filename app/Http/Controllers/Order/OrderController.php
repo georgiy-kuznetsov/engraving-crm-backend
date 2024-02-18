2f5239f7184152890d0a192365c825860d5511cb
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2024-02-18 08:59:28 +0300

    Create the UpdateService for the OrderController

--- app/Http/Controllers/Order/OrderController.php
+++ app/Http/Controllers/Order/OrderController.php
@@ -9,6 +9,7 @@ use App\Models\Coupon;
 use App\Models\Order\Order;
 use App\Models\Product\Product;
 use App\Service\Order\StoreService;
+use App\Service\Order\UpdateService;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
@@ -18,8 +19,9 @@ class OrderController extends Controller
     protected $storeService;
     protected $updateService;
 
-    public function __construct(StoreService $storeService) {
+    public function __construct(StoreService $storeService, UpdateService $updateService) {
         $this->storeService = $storeService;
+        $this->updateService = $updateService;
     }
 
     public function index(Request $request)
@@ -49,99 +51,9 @@ class OrderController extends Controller
                     ->findOrFail($id);
     }
 
-    public function update(UpdateRequest $request, string $id)
+    public function update(UpdateRequest $request, int $id)
     {
-        $order = Order::findOrFail($id);
-
-        $validatedData = $request->validated();
-
-        $productModels = Product::select(['id', 'name', 'photo', 'price', 'sale_price', 'onsale'])
-                            ->whereIn('id', $validatedData['products'])
-                            ->get();
-        $billetModels = Product::select(['id', 'name', 'photo', 'price', 'sale_price', 'onsale'])
-                            ->whereIn('id', $validatedData['products'])
-                            ->get();
-        if ($validatedData['coupon_id']) {
-            $coupon = Coupon::findOrFail($validatedData['coupon_id']);
-        }
-
-        $products = [];
-        foreach ($validatedData['products'] as $key => $productId) {
-            $product = $productModels->find($productId);
-
-            $productQuantity = $validatedData['products_quantity'][$key];
-            $amount = ($product->onsale)
-                        ? $product->sale_price * $productQuantity
-                        : $product->price * $productQuantity;
-            $products[$productId] = [
-                'name' => $product->name,
-                'photo' => $product->photo,
-                'price' => $product->price,
-                'sale_price' => $product->sale_price,
-                'onsale' => $product->onsale,
-                'quantity' => $productQuantity,
-                'amount' => $amount,
-            ];
-        }
-
-        $billets = [];
-        foreach ($validatedData['billets'] as $key => $billetId) {
-            $billet = $billetModels->find($billetId);
-
-            $billetQuantity = $validatedData['billets_quantity'][$key];
-            $amount = ($billet->onsale)
-                        ? $billet->sale_price * $billetQuantity
-                        : $billet->price * $billetQuantity;
-            $billets[$billetId] = [
-                'name' => $billet->name,
-                'photo' => $billet->photo,
-                'price' => $billet->price,
-                'onsale' => $billet->onsale,
-                'quantity' => $billetQuantity,
-                'amount' => $amount,
-            ];
-        }
-
-        $cost = array_reduce($products, function($carry, $item) {
-            $carry += $item['price'] * $item['quantity'];
-            return  $carry;
-        }, 0);
-
-        $costDiscount = array_reduce($products, function($carry, $item) {
-            if ($item['onsale']) {
-                $carry += ($item['price'] - $item['sale_price']) * $item['quantity'];
-            }
-            return  $carry;
-        }, 0);
-        $costRemainder = $cost - $costDiscount;
-
-        if ( isset($coupon) && Carbon::createFromTimestamp($coupon->expires_at)->isPast() ) {
-            if ($coupon->type === 'percent') {
-                $couponDiscount = ($costRemainder) * ((double) $coupon->discount_size / 100);
-            } else {
-                $couponDiscount = ( $coupon->discount_size >= ($costRemainder) )
-                                    ? $costRemainder
-                                    : $coupon->discount_size;
-            }
-        } else {
-            $couponDiscount = 0;
-        }
-
-        $discountAmount = $costDiscount + $couponDiscount;
-        $totalAmount = $cost - $discountAmount + $validatedData['shipping_amount'] + $validatedData['gratuity_amount'];
-
-        $order->update([
-            ...$validatedData,
-            'price_amount' => $cost,
-            'price_discount' => $costDiscount,
-            'discount_amount' => $discountAmount,
-            'total_amount' => $totalAmount,
-        ]);
-
-        $order->products()->sync($products);
-        $order->billets()->sync($billets);
-
-        return $order->load(['status', 'paymentStatus', 'coupon', 'paymentMethod', 'shippingMethod', 'customer', 'products', 'billets', 'source']);
+        return ($this->updateService)($request, $request->validated(), $id);
     }
 
     public function destroy(string $id)
@@ -153,21 +65,4 @@ class OrderController extends Controller
         $order->delete();
         return response()->json([], 204);
     }
-}
-
-function getArrForAttach(array $itemsIds, array $itemsQuantities, Collection $itemsModels): array
-{
-    $pivots = [];
-    foreach ($itemsIds as $key => $productId) {
-        $product = $itemsModels->find($productId);
-        $pivots[$productId] = [
-            'name' => $product->name,
-            'photo' => $product->photo,
-            'price' => $product->price,
-            'sale_price' => $product->sale_price,
-            'onsale' => $product->onsale,
-            'quantity' => $itemsQuantities[$key],
-        ];
-    }
-    return $pivots;
 }
