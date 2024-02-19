f5543a1d7ab32e011f093db5a5fa50182ee075e1
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2024-02-19 14:47:02 +0600

    Refactor the OrderStatusController

--- app/Http/Controllers/OrderStatusController.php
+++ app/Http/Controllers/OrderStatusController.php
@@ -9,34 +9,24 @@ class OrderStatusController extends Controller
 {
     public function index()
     {
-        $status = OrderStatus::all();
-        return $status;
+        return OrderStatus::all();
     }
 
     public function store(OrderStatusRequest $request)
     {
-        $validatedData = $request->validated();
-
-        $status = OrderStatus::create($validatedData);
-
-        return $status;
+        return OrderStatus::create(  $request->validated());
     }
 
-    public function show(string $id)
+    public function show(int $id)
     {
-        if (! $status = OrderStatus::find($id)) {
-            // return $this->sendErrorResponse(['Status not found'], 404);
-        };
-        return $status;
+        return OrderStatus::findOrFail($id);
     }
 
-    public function destroy(string $id)
+    public function destroy(int $id)
     {
-        if ( ! $status = OrderStatus::find($id) ) {
-            return response()->json([], 204);
-        };
-
-        $status->delete();
+        if ( $status = OrderStatus::find($id) ) {
+            $status->delete();
+        }
         return response()->json([], 204);
     }
 }
