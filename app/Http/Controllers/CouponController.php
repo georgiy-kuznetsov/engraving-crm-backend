a79c43ad300b86501550050d2b2437148f18f6f4
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2024-02-19 12:32:17 +0600

    Create the CouponService for the CouponController

--- app/Http/Controllers/CouponController.php
+++ app/Http/Controllers/CouponController.php
@@ -5,12 +5,17 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CouponRequest;
 use App\Models\Coupon;
-use Carbon\Carbon;
-use Illuminate\Http\Request;
-use Illuminate\Validation\Rule;
+use App\Service\CouponService;
 
 class CouponController extends Controller
 {
+    protected $service;
+
+    public function __construct(CouponService $service)
+    {
+        $this->service = $service;
+    }
+
     public function index()
     {
         return Coupon::all();
@@ -18,16 +23,7 @@ class CouponController extends Controller
 
     public function store(CouponRequest $request)
     {
-        $validatedData = $request->validated();
-
-        $validatedData['expires_at'] = Carbon::parse($validatedData['expires_at']);
-
-        $coupon = Coupon::create([
-            ...$validatedData,
-            'user_id' => $request->user()->id,
-        ]);
-
-        return $coupon;
+        return $this->service->store( $request, $request->validated() );
     }
 
     public function show(int $id)
---
+++ app/Service/CouponService.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Service;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CouponRequest;
+use App\Models\Coupon;
+use Carbon\Carbon;
+
+class CouponService extends Controller {
+    public function store(CouponRequest $request, array $data): Coupon
+    {
+        $data['expires_at'] = Carbon::parse($data['expires_at']);
+        return Coupon::create([
+                ...$data,
+                'user_id' => $request->user()->id,
+            ]);
+    }
+}
