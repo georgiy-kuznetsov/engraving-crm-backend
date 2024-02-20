79baa53d97d05e6c9e41c631d2bb8ab3e0ff9d63
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2024-02-20 20:55:16 +0600

    Add the error messages of the RegistrationRequest

--- app/Http/Requests/Auth/RegistrationRequest.php
+++ app/Http/Requests/Auth/RegistrationRequest.php
@@ -21,4 +21,30 @@ class RegistrationRequest extends FormRequest
             'last_name' => ['nullable', 'string', 'max:100'],
         ];
     }
+
+    public function messages(): array
+    {
+        return [
+            'login.required' => 'Логин не может быть пустым',
+            'login.unique' => 'Такой логин уже занят',
+            'login.min' => 'Логин должен быть не менее 7 символов',
+            'login.max' => 'Логин должен быть не более 50 символов',
+            'email.unique' => 'Такой email уже используется',
+            'email.required' => 'Email не может быть пустым',
+            'email.string' => 'Email должен быть строкой',
+            'email.email' => 'Некорректный email',
+            'password.required' => 'Пароль обязателен',
+            'password.confirmed' => 'Пароли не совпадают',
+            'password.min' => 'Пароль должен быть не менее 6 символов',
+            'password.max' => 'Пароль должен быть не более 50 символов',
+            'password.string' => 'Пароль должен быть строкой',
+            'first_name.max' => 'Имя должно быть не более 100 символов',
+            'first_name.string' => 'Имя должно быть строкой',
+            'first_name.max' => 'Имя должно быть не более 100 символов',
+            'last_name.max' => 'Фамилия должна быть не более 100 символов',
+            'last_name.string' => 'Фамилия должна быть строкой',
+            'last_name.max' => 'Фамилия должна быть не более 100 символов',
+
+        ];
+    }
 }
