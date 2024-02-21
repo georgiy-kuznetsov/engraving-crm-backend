131d897d383b2b9f0b401bc6cb5f7c9165cb94e7
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2024-02-21 14:36:12 +0600

    Add error messages for UpdateRequest of CustomerController

--- app/Http/Requests/Customer/UpdateRequest.php
+++ app/Http/Requests/Customer/UpdateRequest.php
@@ -16,7 +16,7 @@ class UpdateRequest extends FormRequest
         return [
             'first_name' => ['required', 'string', 'max:255'],
             'last_name' => ['nullable', 'string', 'max:255'],
-            'phone' => ['nullable', 'string', 'max:255', 'unique:customers,phone,' . $this->id, 'max:100'],
+            'phone' => ['nullable', 'string', 'max:255', 'unique:customers,phone,' . $this->id],
             'email' => ['required', 'string', 'email', 'unique:customers,email,' . $this->id, 'max:100'],
 
             'country' => ['nullable', 'string', 'max:255'],
@@ -30,6 +30,49 @@ class UpdateRequest extends FormRequest
             'telegram' => ['nullable', 'string', 'unique:customers,telegram,' . $this->id, 'max:255'],
             'vkontakte' => ['nullable', 'string', 'unique:customers,vkontakte,' . $this->id, 'max:255'],
             'instagram' => ['nullable', 'string', 'unique:customers,instagram,' . $this->id, 'max:255'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'first_name.required' => 'Имя не может быть пустым',
+            'first_name.string' => 'Имя должно быть строкой',
+            'first_name.max' => 'Имя не может быть больше 255 символов',
+            'last_name.string' => 'Фамилия должна быть строкой',
+            'last_name.max' => 'Фамилия не может быть больше 255 символов',
+            'email.unique' => 'Покупатель с таким email уже существует',
+            'email.string' => 'Email должен быть строкой',
+            'email.email' => 'Введен некорректный email',
+            'email.max' => 'Email не может быть больше 100 символов',
+            'phone.string' => 'Телефон должен быть строкой',
+            'phone.max' => 'Телефон не может быть больше 100 символов',
+            'phone.unique' => 'Такой телефон уже существует',
+            'country.string' => 'Страна должна быть строкой',
+            'country.max' => 'Страна не может быть больше 255 символов',
+            'region.string' => 'Регион должен быть строкой',
+            'region.max' => 'Регион не может быть больше 255 символов',
+            'city.string' => 'Город должен быть строкой',
+            'city.max' => 'Город не может быть больше 255 символов',
+            'adress.string' => 'Адрес должен быть строкой',
+            'adress.max' => 'Адрес не может быть больше 255 символов',
+            'postcode.string' => 'Почтовый индекс должен быть строкой',
+            'postcode.max' => 'Почтовый индекс не может быть больше 20 символов',
+            'website.string' => 'Сайт должен быть строкой',
+            'website.string' => 'Покупатель с таким сайтом уже существует',
+            'website.max' => 'Сайт не может быть больше 255 символов',
+            'telegram.string' => 'Телеграм должен быть строкой',
+            'telegram.string' => 'Покупатель с таким Telegram уже существует',
+            'telegram.max' => 'Телеграм не может быть больше 255 символов',
+            'vkontakte.string' => 'Вконтакте должен быть строкой',
+            'vkontakte.string' => 'Покупатель с таким id Вконтакте уже существует',
+            'vkontakte.max' => 'Вконтакте не может быть больше 255 символов',
+            'instagram.string' => 'Инстаграм должен быть строкой',
+            'instagram.string' => 'Покупатель с таким id Инстаграм уже существует',
+            'instagram.max' => 'Инстаграм не может быть больше 255 символов',
+            'whatsapp.string' => 'Whatsapp должен быть строкой',
+            'whatsapp.unique' => 'Покупатель с таким Whatsapp уже существует',
+            'whatsapp.max' => 'Whatsapp не может быть больше 255 символов',
         ];
     }
 }
