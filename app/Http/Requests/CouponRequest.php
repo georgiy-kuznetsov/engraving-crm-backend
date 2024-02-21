4f1ffd3be4b9e834f5859f681b8b71d888a986e8
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2024-02-21 17:59:36 +0600

    Add error messages for CouponRequest

--- app/Http/Requests/CouponRequest.php
+++ app/Http/Requests/CouponRequest.php
@@ -22,4 +22,23 @@ class CouponRequest extends FormRequest
             'expires_at' => ['required', 'date'],
         ];
     }
+
+    public function messages(): array
+    {
+        return [
+            'promocode.required' => 'Поле обязательно для заполнения',
+            'promocode.unique' => 'Купон с таким кодом уже существует',
+            'promocode.string' => 'Поле должно быть строкой',
+            'promocode.max' => 'Максимальная длина 255 символов',
+            'discount_size.required' => 'Поле обязательно для заполнения',
+            'discount_size.decimal' => 'Поле должно быть числом',
+            'discount_size.max' => 'Максимальное значение 99999999.99',
+            'type.required' => 'Поле обязательно для заполнения',
+            'type.string' => 'Поле должно быть строкой',
+            'type.in' => 'Недопустимое значение',
+            'type.string' => 'Поле должно быть строкой',
+            'expires_at.required' => 'Поле обязательно для заполнения',
+            'expires_at.date' => 'Поле должно быть датой',
+        ];
+    }
 }
