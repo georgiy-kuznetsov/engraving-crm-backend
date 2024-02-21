e2a24fa52a4810218d233dfc5936f8c9c713121e
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2024-02-21 18:06:26 +0600

    Add error messages for OrderSourceRequest

--- app/Http/Requests/OrderSourceRequest.php
+++ app/Http/Requests/OrderSourceRequest.php
@@ -19,4 +19,16 @@ class OrderSourceRequest extends FormRequest
             'index' => ['nullable', 'integer', 'min:0'],
         ];
     }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Поле обязательно для заполнения',
+            'name.max' => 'Максимальное количество символов 255',
+            'name.string' => 'Поле должно быть строкой',
+            'description.string' => 'Поле должно быть строкой',
+            'index.integer' => 'Поле должно быть целым числом',
+            'index.min' => 'Индекс не может быть отрицательным',
+        ];
+    }
 }
