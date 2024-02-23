17a0c8f62e8c215ea4205b5ca5c114aa9970fc02
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2024-02-23 14:54:32 +0600

    Create policy for Provider

--- app/Http/Controllers/ProviderController.php
+++ app/Http/Controllers/ProviderController.php
@@ -18,6 +18,8 @@ class ProviderController extends Controller
 
     public function index(Request $request)
     {
+        $this->authorize('viewAny', Provider::class);
+
         $pageSize = (int) $request->input('pageSize') ?? env('API_ITEMS_PER_PAGE');
         $page = (int) $request->input('page') ?? 1;
 
@@ -34,21 +36,25 @@ class ProviderController extends Controller
 
     public function store(StoreRequest $request)
     {
+        $this->authorize('create', Provider::class);
         return $this->service->store( $request, $request->validated() );
     }
 
     public function show(int $id)
     {
+        $this->authorize('view', Provider::class);
         return Provider::findOrFail($id);
     }
 
     public function update(UpdateRequest $request, string $id)
     {
+        $this->authorize('update', Provider::class);
         return $this->service->update( $request->validated(), $id );
     }
 
     public function destroy(int $id)
     {
+        $this->authorize('delete', Provider::class);
         if ( $provider = Provider::find($id) ) {
             $provider->delete();
         };
---
+++ app/Policies/ProviderPolicy.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Provider;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class ProviderPolicy
+{
+    public function viewAny(User $user): Response
+    {
+        return ($user->role === 'user')
+                    ? Response::deny('У вас нет доступа для выполнения запроса')
+                    : Response::allow();
+    }
+
+    public function view(User $user, Provider $provider): bool
+    {
+        if ($provider->user->id === $user->id) {
+            return true;
+        }
+
+        if ($user->role === 'owner' || $user->role === 'admin' || $user->role === 'manager') {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    public function update(User $user, Provider $provider): bool
+    {
+        if ($provider->user->id === $user->id) {
+            return true;
+        }
+
+        if ($user->role === 'owner' || $user->role === 'admin' || $user->role === 'manager') {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function delete(User $user, Provider $provider): bool
+    {
+        if ($provider->user->id === $user->id) {
+            return true;
+        }
+
+        if ($user->role === 'owner' || $user->role === 'admin' || $user->role === 'manager') {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function restore(User $user, Provider $provider): bool
+    {
+        if ($provider->user->id === $user->id) {
+            return true;
+        }
+
+        if ($user->role === 'owner' || $user->role === 'admin') {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function forceDelete(User $user, Provider $provider): bool
+    {
+        return ($user->role === 'owner' || $user->role === 'admin')
+                    ? true
+                    : false;
+    }
+}
