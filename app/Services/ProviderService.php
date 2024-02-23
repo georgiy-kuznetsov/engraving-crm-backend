9a6801be8e7555032ea4a9aefd57b30e301449d8
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2024-02-23 20:41:29 +0600

    Add index method to ProviderService

--- app/Services/ProviderService.php
+++ app/Services/ProviderService.php
@@ -3,10 +3,31 @@ namespace App\Services;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Provider\StoreRequest;
-use App\Http\Requests\Provider\UpdateRequest;
 use App\Models\Provider;
 
 class ProviderService extends Controller {
+    public function index(array $data): array
+    {
+        $pageSize = $data['pageSize'] ?? env('API_ITEMS_PER_PAGE');
+        $page = $data['page'] ?? 1;
+
+        $userRole = auth()->user()->role->guard_name;
+        if ( in_array($userRole, ['owner', 'admin', 'manager']) ) {
+            $providers = Provider::orderBy('id')->paginate( $pageSize, ['*'], 'page', $page );
+        }
+        else {
+            $providers = Provider::where('user_id', auth()->user()->id)->paginate( $pageSize, ['*'], 'page', $page );
+        }
+
+
+        return [
+            'currentPage' => $providers->currentPage(),
+            'lastPage' => $providers->lastPage(),
+            'pageSize' => $providers->perPage(),
+            'total' => $providers->total(),
+            'items' => $providers->items(),
+        ];
+    }
     public function store(StoreRequest $request, array $data): Provider
     {
         return Provider::create([
