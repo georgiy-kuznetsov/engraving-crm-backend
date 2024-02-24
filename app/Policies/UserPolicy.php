3337e88b1d84d9aa61249070ba85898d20b7ed56
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2024-02-24 14:00:33 +0600

    Refactor UserPolicy

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -9,81 +9,84 @@ class UserPolicy
 {
     public function viewAny(User $user): bool
     {
-        return ( in_array($user->role, ['owner', 'admin']) )
+        return ( in_array( auth()->user()->role->guard_name, ['owner', 'admin']) )
                     ? true
                     : false;
     }
 
     public function view(User $user): bool
     {
-        if ( auth()->user->id === $user->id ) {
+        $currentUser = auth()->user();
+        if ($currentUser->id === $user->id ) {
             return true;
         }
 
-        return ( in_array($user->role, ['owner', 'admin']) )
+        return ( in_array($currentUser->role->guard_name, ['owner', 'admin']) )
                     ? true
                     : false;
     }
 
-    public function create(User $user): bool
+    public function create(): bool
     {
-        return ( in_array($user->role, ['owner', 'admin']) )
+        return ( in_array( auth()->user()->role->guard_name, ['owner', 'admin']) )
                     ? true
                     : false;
     }
 
     public function update(User $user, User $model): bool
     {
-        if ( auth()->user->id === $user->id ) {
+        $currentUser = auth()->user();
+        if ( $currentUser->id === $user->id ) {
             return true;
         }
-
-        if ( $user->role === 'owner' && auth()->user->role !== 'owner' ) {
+        if ( $user->role->guard_name === 'owner' && $currentUser->role->guard_name !== 'owner' ) {
             return false;
         }
-
-        return ( in_array($user->role, ['owner', 'admin']) )
+        return ( in_array($currentUser->role->guard_name, ['owner', 'admin']) )
                     ? true
                     : false;
     }
 
     public function delete(User $user, User $model): bool
     {
-        if ( auth()->user->id === $user->id ) {
+        $currentUser = auth()->user();
+        if ( $currentUser->id === $user->id ) {
             return true;
         }
 
-        if ( $user->role === 'owner' && auth()->user->role !== 'owner' ) {
+        if ( $user->role->guard_name === 'owner' && $currentUser->role->guard_name !== 'owner' ) {
             return false;
         }
 
-        return ( in_array($user->role, ['owner', 'admin']) )
+        return ( in_array($currentUser->role->guard_name, ['owner', 'admin']) )
                     ? true
                     : false;
     }
 
     public function restore(User $user, User $model): bool
     {
-        if ( auth()->user->id === $user->id ) {
+        $currentUser = auth()->user();
+        if ( $currentUser->id === $user->id ) {
             return true;
         }
 
-        if ( $user->role === 'owner' && auth()->user->role !== 'owner' ) {
+        if ( $user->role->guard_name === 'owner' && $currentUser->role->guard_name !== 'owner' ) {
             return false;
         }
 
-        return ( in_array($user->role, ['owner', 'admin']) )
+        return ( in_array($currentUser->role->guard_name, ['owner', 'admin']) )
                     ? true
                     : false;
     }
 
     public function forceDelete(User $user, User $model): bool
     {
-        if ( $user->role === 'owner' && auth()->user->role !== 'owner' ) {
+        $currentUser = auth()->user();
+        if ( $user->role->guard_name === 'owner' && $currentUser->role->guard_name !== 'owner' ) {
             return false;
         }
 
-        return ( in_array($user->role, ['owner', 'admin']) )
+        return ( in_array($currentUser->role->guard_name, ['owner', 'admin']) )
                     ? true
                     : false;
     }
