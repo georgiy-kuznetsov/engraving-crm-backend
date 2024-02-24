239578b29411401cff3828b9c393b2a60e3b5365
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2024-02-24 10:29:00 +0600

    Add index method to BilletService

--- app/Services/Billet/BilletService.php
+++ app/Services/Billet/BilletService.php
@@ -6,6 +6,21 @@ use App\Http\Requests\Billet\StoreRequest;
 use App\Models\Billet;
 
 class BilletService extends Controller {
+    public function index(array $data): array
+    {
+        $page = $data['page'] ?? 1;
+        $pageSize = $data['pageSize'] ?? env('API_ITEMS_PER_PAGE');
+
+        $billetItems = Billet::paginate($pageSize, ['*'], 'page', $page);
+
+        return [
+            'page' => $billetItems->currentPage(),
+            'pageSize' => $billetItems->perPage(),
+            'total' => $billetItems->total(),
+            'items' => $billetItems->items(),
+        ];
+    }
+
     public function store(StoreRequest $request, array $data): Billet
     {
         return Billet::firstOrCreate([
