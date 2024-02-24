70fae5108bd18b65f0051826836cfb911c84da7d
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2024-02-24 11:20:17 +0600

    Create policy for customer model

---
+++ app/Policies/CustomerPolicy.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Customer;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class CustomerPolicy
+{
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    public function view(User $user, Customer $customer): bool
+    {
+        return true;
+    }
+
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    public function update(User $user, Customer $customer): bool
+    {
+        return ( in_array($user->role, ['owner', 'admin']) )
+                    ? true
+                    : false;
+    }
+
+    public function delete(User $user, Customer $customer): bool
+    {
+        return ( in_array($user->role, ['owner', 'admin']) )
+                    ? true
+                    : false;
+    }
+
+    public function restore(User $user, Customer $customer): bool
+    {
+        return ( in_array($user->role, ['owner', 'admin']) )
+                    ? true
+                    : false;
+    }
+
+    public function forceDelete(User $user, Customer $customer): bool
+    {
+        return ( in_array($user->role, ['owner', 'admin']) )
+                    ? true
+                    : false;
+    }
+}
