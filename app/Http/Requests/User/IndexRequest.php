462c44b018b3d36542d73853e43286d70eaad200
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2024-02-24 13:28:34 +0600

    Create IndexRequest for UserController

---
+++ app/Http/Requests/User/IndexRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class IndexRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'pageSize' => ['nullable', 'integer', 'max:100'],
+            'page' => ['nullable', 'integer', 'max:999'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'pageSize.max' => 'Поле должно быть не больше 100',
+            'pageSize.integer' => 'Поле должно быть числом',
+            'page.max' => 'Поле должно быть не больше 999',
+            'page.integer' => 'Поле должно быть числом',
+        ];
+    }
+}
