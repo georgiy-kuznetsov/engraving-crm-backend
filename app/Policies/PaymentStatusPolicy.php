6873421cb2604594615227e10917ff59a6575fa8
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2024-02-24 12:57:15 +0600

    Create policy for PaymentStatus model

---
+++ app/Policies/PaymentStatusPolicy.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\PaymentStatus;
+use App\Models\User;
+
+class PaymentStatusPolicy
+{
+    public function viewAny(): bool
+    {
+        return true;
+    }
+
+    public function view(): bool
+    {
+        return true;
+    }
+
+    public function create(User $user): bool
+    {
+        return ( in_array($user->role, ['owner', 'admin']) )
+                    ? true
+                    : false;
+    }
+
+    public function update(User $user): bool
+    {
+        return ( in_array($user->role, ['owner', 'admin']) )
+                    ? true
+                    : false;
+    }
+
+    public function delete(User $user): bool
+    {
+        return ( in_array($user->role, ['owner', 'admin']) )
+                    ? true
+                    : false;
+    }
+
+    public function restore(User $user): bool
+    {
+        return ( in_array($user->role, ['owner', 'admin']) )
+                    ? true
+                    : false;
+    }
+
+    public function forceDelete(User $user): bool
+    {
+        return ( in_array($user->role, ['owner', 'admin']) )
+                    ? true
+                    : false;
+    }
+}
