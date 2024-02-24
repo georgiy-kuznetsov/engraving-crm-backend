d6479dba3ec092ca22dbe24d701aff7e15f63a2a
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2024-02-24 13:25:17 +0600

    Refactor IndexService of UserController

--- app/Services/User/IndexService.php
+++ app/Services/User/IndexService.php
@@ -4,7 +4,7 @@ namespace App\Services\User;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 
-class StoreService extends Controller {
+class IndexService extends Controller {
     public function __invoke(array $data): array
     {
         $page = $data['page'] ?? 1;
