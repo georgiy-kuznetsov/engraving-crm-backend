cac67a1bcced19ce8ead2e50b7435e30f5f0db40
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2024-02-24 12:28:55 +0600

    Create policy for OrderSource model

---
+++ app/Policies/OrderSourcePolicy.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\OrderSource;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class OrderSourcePolicy
+{
+    public function viewAny(): bool
+    {
+        return true;
+    }
+
+    public function view(): bool
+    {
+        return true;
+    }
+
+    public function create(User $user): bool
+    {
+        return ( in_array($user->role, ['owner', 'admin']) )
+                    ? true
+                    : false;
+    }
+
+    public function update(User $user): bool
+    {
+        return ( in_array($user->role, ['owner', 'admin']) )
+                    ? true
+                    : false;
+    }
+
+    public function delete(User $user): bool
+    {
+        return ( in_array($user->role, ['owner', 'admin']) )
+                    ? true
+                    : false;
+    }
+
+    public function restore(User $user): bool
+    {
+        return ( in_array($user->role, ['owner', 'admin']) )
+                    ? true
+                    : false;
+    }
+
+    public function forceDelete(User $user): bool
+    {
+        return ( in_array($user->role, ['owner', 'admin']) )
+                    ? true
+                    : false;
+    }
+}
