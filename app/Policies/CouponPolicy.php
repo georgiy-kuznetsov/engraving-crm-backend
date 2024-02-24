7ae5aecd489ea8424d81fd7ae4f79fe65eb5ea78
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2024-02-24 11:13:04 +0600

    Create policy for coupon model

---
+++ app/Policies/CouponPolicy.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Coupon;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class CouponPolicy
+{
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    public function view(User $user, Coupon $coupon): bool
+    {
+        return true;
+    }
+
+    public function create(User $user): bool
+    {
+        return ( in_array($user->role, ['owner', 'admin']) )
+                    ? true
+                    : false;
+    }
+
+    public function update(User $user, Coupon $coupon): bool
+    {
+        return ( in_array($user->role, ['owner', 'admin']) )
+                    ? true
+                    : false;
+    }
+
+    public function delete(User $user, Coupon $coupon): bool
+    {
+        return ( in_array($user->role, ['owner', 'admin']) )
+                    ? true
+                    : false;
+    }
+
+    public function restore(User $user, Coupon $coupon): bool
+    {
+        return ( in_array($user->role, ['owner', 'admin']) )
+                    ? true
+                    : false;
+    }
+
+    public function forceDelete(User $user, Coupon $coupon): bool
+    {
+        return ( in_array($user->role, ['owner', 'admin']) )
+                    ? true
+                    : false;
+    }
+}
