392c0c0c59e14b74c37257590de9f7da87ae7b05
George Kuznetsov <georgiy.kuznetsov.19@mail.ru>
2024-02-24 11:27:08 +0600

    Add index method to CustomerService

--- app/Services/CustomerService.php
+++ app/Services/CustomerService.php
@@ -6,6 +6,21 @@ use App\Http\Requests\Customer\StoreRequest;
 use App\Models\Customer;
 
 class CustomerService extends Controller {
+    public function index(array $data): array
+    {
+        $page = $data['page'] ?? 1;
+        $pageSize = $data['pageSize'] ?? env('API_ITEMS_PER_PAGE');
+
+        $customerItems = Customer::paginate($pageSize, ['*'], 'page', $page);
+
+        return [
+            'page' => $customerItems->currentPage(),
+            'pageSize' => $customerItems->perPage(),
+            'total' => $customerItems->total(),
+            'items' => $customerItems->items(),
+        ];
+    }
+
     public function store(StoreRequest $request, array $data): Customer
     {
         return Customer::create([
